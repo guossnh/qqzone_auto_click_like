b1282b5c035a8bcc1562fbeabbe05cbd1b8a964c
tlk <guossnh@gmail.com>
2016-05-02 22:14:20 +0800

    one error try it on win

---
+++ man.js
@@ -0,0 +1,2 @@
+//qq kong jian zi dong dian zan
+var zan = document.getElementsByClassName("item qz_like_btn_v3");
--- man.py
+++ man.py
@@ -7,8 +7,12 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
+
+#
 driver = webdriver.Firefox()
-driver.get("https://www.baidu.com")
-assert "百度一下，你就知道" in driver.title
-time.sleep(5)
+driver.get("http://user.qzone.qq.com/")
+assert "QQ空间-分享生活，留住感动" in driver.title
+time.sleep(3)
+#driver.execute_script("document.getElementById('switcher_plogin').click()")
+driver.find_element_by_id("switcher_plogin").click()
 driver.close()
